d527829fdd9fbc2a51dad17f0ab147af65b5b4f9
jesse51002 <jessemusa2@gmail.com>
2024-06-10 05:23:19 +0000

    added clip quality interations

--- bash.sh
+++ bash.sh
@@ -6,16 +6,12 @@
 #    --W_steps=1000 --FS_steps=200  \
 #    --hair_perc_lambda 0 --body_alternate_number 5 --opt_name adam
 
-#python main.py --im_path1 IMG_3248.png --im_path2 shoulder_length_wavy_wispy_bangs.png --im_path3 shoulder_length_wavy_wispy_bangs.png \
-#    --sign realistic  --smooth 5 --learning_rate 0.05 \
-#    --align_steps1 80 --align_steps2 120 --blend_steps 100 \
-#    --W_steps=300 --FS_steps=150  \
-#    --style_lambda 200000 --body_alternate_number 5 --align_color_lambda 0.0001
-
-    
-python main.py --im_path1 IMG_3248.png --im_path2 shoulder_length_wavy_wispy_bangs.png --im_path3 shoulder_length_wavy_wispy_bangs.png \
-    --model StyleGanXL --ckpt 'pretrained_models/ffhq1024xl.pkl'\
+python main.py --im_path1 IMG_4302.png --im_path2 werewolf.png --im_path3 werewolf.png \
     --sign realistic  --smooth 5 --learning_rate 0.05 \
-    --align_steps1 1 --align_steps2 1 --blend_steps 1 \
-    --W_steps=200 --FS_steps=1  \
-    --style_lambda 200000 --body_alternate_number 5 --align_color_lambda 0.0001
+    --align_steps1 80 --align_steps2 120 --blend_steps 100 \
+    --W_steps=300 --FS_steps=150  \
+    --style_lambda 200000 --body_alternate_number 5 --align_color_lambda 0.0001 \
+    --clip_quality --clip_quality_iterations 20
+
+# Time with no clip: 13 seconds
+# Time with 20 iterations clip:  15 seconds
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from models.FacerParsing import FacerModel, FacerKeypoints, FacerDetection
 from models.p3m_matting.inference import human_matt_model
 from models.Net import Net
 from models.NetXL import NetXL
+from models.QualityClip import QualityClip
 
 
 def main(args):
@@ -62,6 +63,8 @@ def main(args):
         net1 = NetXL(args, device=args.device[1]) if args.is_multi_gpu else None
     else:
         raise NotImplementedError(f"{args.model} is not implemented in this code base")
+
+    quality_clip = QualityClip(device=args.device[0]) if args.clip_quality else None
         
     face_detector = FacerDetection(device=args.device[1] if args.is_multi_gpu else args.device[0])
     keypoint_model = FacerKeypoints(face_detector=face_detector, device=args.device[1] if args.is_multi_gpu else args.device[0])
@@ -70,7 +73,7 @@ def main(args):
 
     ii2s = Embedding(args, net=net0)
     segmentor = SegMaker(args, facer=facer, background_remover=background_remover, keypoint_model=keypoint_model)
-    align = Alignment(args, seg0=seg0, seg1=seg1, net0=net0, net1=net1)
+    align = Alignment(args, seg0=seg0, seg1=seg1, net0=net0, net1=net1, quality_clip=quality_clip)
     blend = Blending(args, seg=seg0, net=net0, facer=facer, background_remover=background_remover)
     print("Finished loading models")
 
